0dd711c27e2dfd12d78d9651f6f3b8c68494c2a1
Juniper Chapman <juniper@wl-80-38.wireless.umt.edu>
2015-10-16 15:17:58 -0600

    things seem to work

--- routes/animal.js
+++ routes/animal.js
@@ -4,33 +4,41 @@ var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 router.use(bodyParser.urlencoded({ extended: true }))
 
+router.use(function(req, res, next) {
+    console.log('Something is happening.');
+    next();
+})
 
 router.route('/')
 
-/* GET All Blogs */
+/* GET All Animals */
   .get(function(req, res) {
     mongoose.model('Animal').find({}, function(err, animals){
       if(err){
-        //DO SOMETHING
+        res.send("Hey bear-fucker! Do you require assistance?");
       } else {
-        //DO SOMETHING WITH THE RESPONSE
+        console.log("Get is working")
+        res.json(animals);
       }
     });
   })
 
   .post(function(req, res){
-
-    //save the request variables
+    var type = req.body.type;
+    var name = req.body.name; 
 
     mongoose.model('Animal').create({
-      //Use the variables
+      type:type,
+      name:name
     }, function(err, animal){
       if(err){
-        //DO SOMETHING
+        res.send("Hey bear-fucker! Do you require assistance?");
       } else{
-        //DO SOMETHING WITH THE RESPONSE
+        res.json({message:'Animal created!'});
       }
     });
   });
 
 module.exports = router;
+
+
